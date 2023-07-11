500920dce2cdb3004aa043ae2d2eb2af8f0a3871
Vjfrib <107771171+Vjfrib@users.noreply.github.com>
2023-07-11 15:12:05 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const rls = require("readline-sync")
+
+
+function exibirMenu (){
+
+    console.clear();
+
+    console.log("splah ROUGE LIKE");
+    console.log("1 - Jogar");
+    console.log("2 - Dificuldade");
+    console.log("3 - Ranking");
+    console.log("4 - Sair");
+}
+
+function funcMenu (indice){
+    
+    if (indice === 1){
+        console.clear();
+        console.log("foi pro jogo");
+    }
+    else{
+        console.log("saiu");
+    }
+}
+do {
+//chamada 1
+exibirMenu();
+var interruptor = rls.questionInt("\n Digite sua option: ")
+//chamada 2
+funcMenu(interruptor);
+}while( interruptor != 1)
